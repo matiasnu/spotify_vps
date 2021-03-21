6f703e226aa0a5e3b0d1bc2e44b732f6d55e7f6e
matias.nunez <matias.nunez@intraway.com>
2021-03-21 16:39:32 -0300

    Refactor config server

--- config_server.sh
+++ config_server.sh
@@ -6,7 +6,7 @@ username=
 password=
 
 # Install go in the server in /root path
-func golang_install() {
+golang_install() {
     cd ~
     curl -O https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz
     tar -xvf go1.12.1.linux-amd64.tar.gz -C /usr/local 
@@ -17,7 +17,7 @@ func golang_install() {
 }
 
 # Clone spotify_service in the server
-func build_service () {
+build_service () {
     cd /root/go/src/
     git clone https://${username}:${password}@github.com/guidoperre/spotify-service.git
     cd spotify-service
@@ -26,7 +26,7 @@ func build_service () {
 }
 
 # Clone spotify_vps in the server
-func build_environment_spotify() {
+build_environment_spotify() {
     cd /home/truchada/
     git clone -b only_spotify https://${username}:${password}@github.com/Matiasnu/spotify_vps.git
     cd spotify_vps/environment
@@ -34,7 +34,7 @@ func build_environment_spotify() {
 }
 
 # Install docker in the server
-func docker_install() {
+docker_install() {
     apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
@@ -45,18 +45,18 @@ func docker_install() {
 }
 
 # Install docker-compose in the server
-func docker_compose_install() {
+docker_compose_install() {
     curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 }
 
-func grafic_install() {
+grafic_install() {
     useradd -m truchada && passwd truchada
     apt update && apt upgrade -y && apt install ubuntu-desktop -y && init 6
 }
 
 # Install basic tools
-func basic_install() {
+basic_install() {
     apt install curl -y
     apt install git -y
 }
